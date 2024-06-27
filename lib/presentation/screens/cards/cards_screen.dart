48ecb9dc89dcfe8477cdedb29093dd13e5c37f37
RAUriel <urielonefusion@gmail.com>
2024-06-27 16:43:32 -0600

    first commit

--- lib/presentation/screens/cards/cards_screen.dart
+++ lib/presentation/screens/cards/cards_screen.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
 
+const cards = <Map<String, dynamic>> {
+  { 'elevation' : 0.0, 'label' : 'Elevation 0' },
+  { 'elevation' : 1.0, 'label' : 'Elevation 1' },
+  { 'elevation' : 2.0, 'label' : 'Elevation 2' },
+  { 'elevation' : 3.0, 'label' : 'Elevation 3' },
+  { 'elevation' : 4.0, 'label' : 'Elevation 4' },
+  { 'elevation' : 5.0, 'label' : 'Elevation 5' },
+};
+
 class CardsSreen extends StatelessWidget {
   const CardsSreen({super.key});
 
@@ -12,3 +21,4 @@ class CardsSreen extends StatelessWidget {
       );
   }
 }
+
